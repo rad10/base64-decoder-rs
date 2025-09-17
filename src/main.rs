f6f3ec40367734daea5425afb2f0a85713843d97
Jabber <ncottrellweb@gmail.com>
2025-09-17 15:51:38 -0400

    Added argument to switch to bruteforce only

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use base64_bruteforcer_rs::{
+use crate::{
     base64_parser::{
         Base64Bruteforcer, BruteforcerTraits, ConvertString, DisplayLines, Permutation,
     },
@@ -9,6 +9,7 @@ use clap::Parser;
 use tool_args::ToolArgs;
 
 mod base64_parser;
+mod phrase_reduction;
 mod phrase_solving;
 mod tool_args;
 
@@ -20,29 +21,33 @@ fn main() {
         .init();
 
     // set base64 string as bytes
-    let example_string = parser.b64_string.as_bytes();
-
-    let string_permutation: Phrase<String> = match parser.use_utf16 {
-        false => {
-            let mut bruteforcer = Base64Bruteforcer::<u8>::default();
-            bruteforcer.collect_combinations(example_string);
-            Phrase::from(bruteforcer)
+    let mut string_permutation: Phrase<String> = if let Some(b64_string) = parser.input.b64_string {
+        match parser.use_utf16 {
+            false => {
+                let mut bruteforcer = Base64Bruteforcer::<u8>::default();
+                bruteforcer.collect_combinations(b64_string.as_bytes());
+                Phrase::from(bruteforcer)
+            }
+            true => {
+                let mut bruteforcer = Base64Bruteforcer::<u16>::default();
+                bruteforcer.collect_combinations(b64_string.as_bytes());
+                Phrase::from(bruteforcer)
+            }
         }
-        true => {
-            let mut bruteforcer = Base64Bruteforcer::<u16>::default();
-            bruteforcer.collect_combinations(example_string);
-            Phrase::from(bruteforcer)
-        }
+    } else if let Some(schema) = parser.input.use_schema {
+        schema
+    } else {
+        unreachable!();
     };
 
-    let mut string_permutation = string_permutation;
-
     if !parser.no_prune {
-        log::info!(
-            "schema: {:?}\n# of permutations: {}",
-            string_permutation.convert_to_string(),
-            string_permutation.permutations(),
-        );
+        if parser.info {
+            println!(
+                "schema: {:?}\n# of permutations: {}",
+                string_permutation.convert_to_string(),
+                string_permutation.permutations(),
+            );
+        }
 
         log::info!("Reducing permutations to logical choices");
         string_permutation.reduce_to_end();
--- src/tool_args.rs
+++ src/tool_args.rs
@@ -1,10 +1,14 @@
-use clap::Parser;
+use std::str::FromStr;
+
+use crate::phrase_reduction::Phrase;
+use clap::{Args, Parser};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 pub(crate) struct ToolArgs {
-    /// Sets the string to brute-force
-    pub(crate) b64_string: String,
+    /// Determines the input type the tool will take
+    #[command(flatten)]
+    pub(crate) input: ActionType,
 
     /// Tells the tools that the string is UTF16
     #[arg(short, long)]
@@ -22,4 +26,26 @@ pub(crate) struct ToolArgs {
 
     #[command(flatten)]
     pub(crate) verbose: clap_verbosity_flag::Verbosity,
+}
+
+#[derive(Args)]
+#[group(required = true, multiple = false)]
+pub(crate) struct ActionType {
+    /// Sets the string to brute-force
+    pub(crate) b64_string: Option<String>,
+
+    /// Takes an already pulled out schema to bruteforce
+    #[arg(short = 's', long)]
+    pub(crate) use_schema: Option<Phrase<String>>,
+}
+
+// Added implementation for fromstring to allow as argument
+
+impl FromStr for Phrase<String> {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let raw_schema: Result<Vec<Vec<String>>, String> = serde_json::from_str(s).map_err(|e| format!("Failed to collect schema: {}", e));
+        raw_schema.and_then(|schema| Ok(Self::from(schema)))
+    }
 }

be654f73df2ff341c6400e038fc7af87eaefd503
Jabber <ncottrellweb@gmail.com>
2025-10-29 23:32:01 -0400

    Updated whatlang function to work with variants

--- src/phrase/validation/mod.rs
+++ src/phrase/validation/mod.rs
@@ -1,9 +1,16 @@
 //! This module handles all functions used to analyze data and determine
 //! validity and accuracy in terms of what the user desires
 
+use std::fmt::Display;
+
+use crate::phrase::schema::Variation;
+
 /// Provides a confidence on a given string using the whatlang library
-pub fn validate_with_whatlang(text: String) -> f64 {
-    match whatlang::detect(text.as_str()) {
+pub fn validate_with_whatlang<T>(text: &Variation<T>) -> f64
+where
+    Variation<T>: Display,
+{
+    match whatlang::detect(text.to_string().as_str()) {
         Some(info) => info.confidence(),
         None => 0.0,
     }

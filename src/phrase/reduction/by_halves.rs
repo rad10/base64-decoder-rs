ebb1be5be9fabf0a52634cd8836328f1eec1316c
Jabber <ncottrellweb@gmail.com>
2025-11-12 22:22:19 -0500

    Redid halves reduction to work with bulk

--- src/phrase/reduction/by_halves.rs
+++ src/phrase/reduction/by_halves.rs
@@ -33,50 +33,59 @@ use std::fmt::{Debug, Display};
 
 use itertools::Itertools;
 
-use crate::phrase::schema::{ConvertString, Permutation, Phrase, Section, Snippet, Variation};
+use crate::phrase::schema::{Permutation, Phrase, Section, Snippet, Variation};
 
 /// Provides an interface to reduce an array like structure to through a
 /// validator utilizing a recursive process
-pub trait ReduceHalves<U, V> {
-    /// Defines the type of item that is collected from the phrase
-    type Item;
-
+pub trait ReduceHalves<'a, T> {
     /// This schema reduction strategy takes the reverse of pairs. While
     /// pairs will start with the smallest group, this function will work
     /// backwards and reduce using the largest valid permutation available.
     /// This largest available permutation will depend on `permutation_limit`
     /// to decide the size of the section.
-    fn reduce_halves(&self, size_checker: U, confidence_interpreter: V) -> Self;
-
-    /// A helper function to [`reduce_halves`]. Takes a binary search
-    /// approach by cutting the sections in half and running the validation
-    /// check on all values in that section if the permutation value is low
-    /// enough. Otherwise, cut it in half and try again.
-    ///
-    /// [`reduce_halves`]: Self::reduce_halves
-    fn reduce_schema_binary(
-        size_checker: &U,
-        phrase_snippet: Snippet<'_, Self::Item>,
-        confidence_interpreter: &V,
-    ) -> Vec<Section<Self::Item>>;
+    fn reduce_halves<'b, V, W>(
+        &'a self,
+        size_checker: &'b V,
+        confidence_interpreter: &'b W,
+    ) -> Self
+    where
+        Self: ReduceHalvesBulk<'a, T, Box<dyn Iterator<Item = (f64, Variation<T>)> + 'b>>,
+        T: 'a,
+        V: Fn(&Snippet<'a, T>) -> bool,
+        W: Fn(&Variation<T>) -> f64 + 'b,
+        'a: 'b;
 
     /// Reduces the phrase until the reduction function cannot reduce it
     /// anymore.
-    fn halves_to_end(&self, size_checker: U, confidence_interpreter: V) -> Self;
+    fn halves_to_end<'b, V, W>(
+        &'a self,
+        size_checker: &'b V,
+        confidence_interpreter: &'b W,
+    ) -> Self
+    where
+        Self: ReduceHalvesBulk<'a, T, Box<dyn Iterator<Item = (f64, Variation<T>)> + 'b>>,
+        T: 'a,
+        V: Fn(&Snippet<'a, T>) -> bool,
+        W: Fn(&Variation<T>) -> f64 + 'b,
+        'a: 'b;
 }
 
 /// Provides an interface to reduce an array like structure to through a
 /// validator utilizing a recursive process
-pub trait ReduceHalvesBulk<U, V> {
-    /// Defines the type of item that is collected from the phrase
-    type Item;
+pub trait ReduceHalvesBulk<'a, T, U> {
+    // /// Defines the type of item that is collected from the phrase
+    // type Item;
 
     /// This schema reduction strategy takes the reverse of pairs. While
     /// pairs will start with the smallest group, this function will work
     /// backwards and reduce using the largest valid permutation available.
     /// This largest available permutation will depend on `permutation_limit`
     /// to decide the size of the section.
-    fn bulk_reduce_halves(&self, size_checker: U, confidence_interpreter: V) -> Self;
+    fn bulk_reduce_halves<V, W>(&'a self, size_checker: V, confidence_interpreter: W) -> Self
+    where
+        T: 'a,
+        V: Fn(&Snippet<'a, T>) -> bool,
+        W: Fn(Snippet<'a, T>) -> U;
 
     /// A helper function to [`bulk_reduce_halves`]. Takes a binary search
     /// approach by cutting the sections in half and running the validation
@@ -84,141 +93,92 @@ pub trait ReduceHalvesBulk<U, V> {
     /// enough. Otherwise, cut it in half and try again.
     ///
     /// [`bulk_reduce_halves`]: Self::bulk_reduce_halves
-    fn bulk_reduce_schema_binary(
-        size_checker: &U,
-        phrase_snippet: Snippet<'_, Self::Item>,
-        confidence_interpreter: &V,
-    ) -> Vec<Section<Self::Item>>;
+    fn bulk_reduce_schema_binary<V, W>(
+        size_checker: V,
+        phrase_snippet: Snippet<'a, T>,
+        confidence_interpreter: W,
+    ) -> Vec<Section<T>>
+    where
+        T: 'a,
+        V: Fn(&Snippet<'a, T>) -> bool,
+        W: Fn(Snippet<'a, T>) -> U;
 
     /// Reduces the phrase until the reduction function cannot reduce it
     /// anymore.
-    fn bulk_halves_to_end(&self, size_checker: U, confidence_interpreter: V) -> Self;
-}
-
-impl<T, U, V> ReduceHalves<U, V> for Phrase<T>
-where
-    T: Debug,
-    U: Fn(&Snippet<'_, T>) -> bool,
-    V: Fn(&Variation<T>) -> f64,
-    Variation<T>: Clone + Display,
-{
-    type Item = T;
-
-    fn reduce_halves(&self, size_checker: U, confidence_interpreter: V) -> Self {
-        Self::new(Self::reduce_schema_binary(
-            &size_checker,
-            Snippet::from(self.sections.as_slice()),
-            &confidence_interpreter,
-        ))
-    }
-
-    fn reduce_schema_binary(
-        size_checker: &U,
-        phrase_snippet: Snippet<'_, Self::Item>,
-        confidence_interpreter: &V,
-    ) -> Vec<Section<Self::Item>> {
-        // Leave early if section is empty or just one
-        if phrase_snippet.len_sections() < 2 {
-            phrase_snippet.sections.to_vec()
-        }
-        // If the permutations within the sections is less than limit, then start crunching through them
-        else if size_checker(&phrase_snippet) {
-            vec![
-                phrase_snippet
-                    .iter_var()
-                    .map(|line| (confidence_interpreter(&line), line))
-                    .inspect(|(confidence, line)| {
-                        log::debug!("confidence, string: {confidence}, {line:?}")
-                    })
-                    // Keeping only square root of permitted permutations to allow rerunning the reduction
-                    .k_largest_relaxed_by_key(
-                        usize::max(phrase_snippet.permutations().sqrt().floor() as usize, 1),
-                        |(confidence, _)| (confidence * 100_000_f64) as usize,
-                    )
-                    .inspect(|(confidence, line)| {
-                        log::debug!("Accepted: confidence, string: {confidence}, {line:?}")
-                    })
-                    .map(|(_, line)| line)
-                    .collect::<Section<T>>(),
-            ]
-        }
-        // If permutations are still too big, split it again
-        else {
-            phrase_snippet
-                .sections
-                .chunks(phrase_snippet.len_sections() / 2)
-                .flat_map(|c| {
-                    Self::reduce_schema_binary(
-                        size_checker,
-                        Snippet::new(c),
-                        confidence_interpreter,
-                    )
-                })
-                .collect()
-        }
-    }
-
-    fn halves_to_end(&self, size_checker: U, confidence_interpreter: V) -> Self
+    fn bulk_halves_to_end<V, W>(
+        &'a self,
+        recursive_val: Option<(usize, usize)>,
+        size_checker: V,
+        confidence_interpreter: W,
+    ) -> Self
     where
-        Variation<T>: Display,
+        T: 'a,
+        V: Fn(&Snippet<'a, T>) -> bool,
+        W: Fn(Snippet<'a, T>) -> U;
+}
+
+impl<'a, T> ReduceHalves<'a, T> for Phrase<T> {
+    fn reduce_halves<'b, V, W>(&'a self, size_checker: &'b V, confidence_interpreter: &'b W) -> Self
+    where
+        Self: ReduceHalvesBulk<'a, T, Box<dyn Iterator<Item = (f64, Variation<T>)> + 'b>>,
+        T: 'a,
+        V: Fn(&Snippet<'a, T>) -> bool,
+        W: Fn(&Variation<T>) -> f64 + 'b,
+        'a: 'b,
     {
-        // Begin by flattening single variation items
-        let mut new_phrase = self.flatten_sections();
-        // Set the last permutation to last size. Stop if permutation doesnt
-        // shrink in any given instance
-        let mut last_size = usize::MAX;
-        while last_size > new_phrase.len_sections() {
-            last_size = new_phrase.len_sections();
-            new_phrase = new_phrase.reduce_halves(&size_checker, &confidence_interpreter);
-            match log::max_level() {
-                log::LevelFilter::Info => {
-                    log::info!(
-                        "Schema: {:?}\n# of permutations: {:e}",
-                        new_phrase.convert_to_string(),
-                        new_phrase.permutations(),
-                    );
-                }
-                x if x >= log::LevelFilter::Debug => {
-                    log::debug!(
-                        "Schema: {:?}\n# of sections: {}\n# of refs: {}\n# of permutations: {:e}",
-                        new_phrase.sections,
-                        new_phrase.len_sections(),
-                        new_phrase.num_of_references(),
-                        new_phrase.permutations(),
-                    );
-                }
-                _ => (),
-            };
-        }
-        new_phrase
+        self.bulk_reduce_halves(size_checker, |snip| {
+            Box::new(
+                snip.into_iter_var()
+                    .map(|line| (confidence_interpreter(&line), line)),
+            )
+        })
+    }
+
+    fn halves_to_end<'b, V, W>(&'a self, size_checker: &'b V, confidence_interpreter: &'b W) -> Self
+    where
+        Self: ReduceHalvesBulk<'a, T, Box<dyn Iterator<Item = (f64, Variation<T>)> + 'b>>,
+        T: 'a,
+        V: Fn(&Snippet<'a, T>) -> bool,
+        W: Fn(&Variation<T>) -> f64 + 'b,
+        'a: 'b,
+    {
+        self.bulk_halves_to_end(None, size_checker, |snip| {
+            Box::new(
+                snip.into_iter_var()
+                    .map(|line| (confidence_interpreter(&line), line)),
+            )
+        })
     }
 }
 
-impl<T, U, V, W> ReduceHalvesBulk<U, V> for Phrase<T>
+impl<'a, T, U> ReduceHalvesBulk<'a, T, U> for Phrase<T>
 where
-    T: Debug,
-    U: Fn(&Snippet<'_, T>) -> bool,
-    V: Fn(&Snippet<'_, T>) -> W,
-    W: Iterator<Item = (f64, Variation<T>)>,
-    Variation<T>: Clone + Display,
+    T: Clone + Debug,
+    U: Iterator<Item = (f64, Variation<T>)>,
+    Variation<T>: Display,
 {
-    type Item = T;
-
-    fn bulk_reduce_halves(&self, size_checker: U, confidence_interpreter: V) -> Self {
+    fn bulk_reduce_halves<V, W>(&'a self, size_checker: V, confidence_interpreter: W) -> Self
+    where
+        T: 'a,
+        V: Fn(&Snippet<'a, T>) -> bool,
+        W: Fn(Snippet<'a, T>) -> U,
+    {
         Self::new(Self::bulk_reduce_schema_binary(
             &size_checker,
-            Snippet::from(self.sections.as_slice()),
-            &confidence_interpreter,
+            self.as_snippet(),
+            confidence_interpreter,
         ))
     }
 
-    fn bulk_reduce_schema_binary(
-        size_checker: &U,
-        phrase_snippet: Snippet<'_, Self::Item>,
-        confidence_interpreter: &V,
-    ) -> Vec<Section<Self::Item>>
+    fn bulk_reduce_schema_binary<V, W>(
+        size_checker: V,
+        phrase_snippet: Snippet<'a, T>,
+        confidence_interpreter: W,
+    ) -> Vec<Section<T>>
     where
-        Variation<T>: Clone,
+        T: 'a + Clone + Debug,
+        V: Fn(&Snippet<'a, T>) -> bool,
+        W: Fn(Snippet<'a, T>) -> U,
     {
         // Leave early if section is empty or just one
         if phrase_snippet.len_sections() < 2 {
@@ -226,14 +186,15 @@ where
         }
         // If the permutations within the sections is less than limit, then start crunching through them
         else if size_checker(&phrase_snippet) {
+            let snippet_permutation = phrase_snippet.permutations();
             vec![
-                confidence_interpreter(&phrase_snippet)
+                confidence_interpreter(phrase_snippet)
                     .inspect(|(confidence, line)| {
                         log::debug!("confidence, string: {confidence}, {line:?}")
                     })
                     // Keeping only square root of permitted permutations to allow rerunning the reduction
                     .k_largest_relaxed_by_key(
-                        usize::max(phrase_snippet.permutations().sqrt().floor() as usize, 1),
+                        usize::max(snippet_permutation.sqrt().floor() as usize, 1),
                         |(confidence, _)| (confidence * 100_000_f64) as usize,
                     )
                     .inspect(|(confidence, line)| {
@@ -250,49 +211,49 @@ where
                 .chunks(phrase_snippet.len_sections() / 2)
                 .flat_map(|c| {
                     Self::bulk_reduce_schema_binary(
-                        size_checker,
+                        &size_checker,
                         Snippet::new(c),
-                        confidence_interpreter,
+                        &confidence_interpreter,
                     )
                 })
                 .collect()
         }
     }
 
-    fn bulk_halves_to_end(&self, size_checker: U, confidence_interpreter: V) -> Self
+    fn bulk_halves_to_end<V, W>(
+        &'a self,
+        recursive_val: Option<(usize, usize)>,
+        size_checker: V,
+        confidence_interpreter: W,
+    ) -> Self
     where
-        T: Debug,
+        T: 'a + Clone + Debug,
+        V: Fn(&Snippet<'a, T>) -> bool,
+        W: Fn(Snippet<'a, T>) -> U,
         Variation<T>: Display,
     {
-        // Begin by flattening single variation items
-        let mut new_phrase = self.flatten_sections();
-        // Set the last permutation to last size. Stop if permutation doesnt
-        // shrink in any given instance
-        let mut last_size = usize::MAX;
-        while last_size > new_phrase.len_sections() {
-            last_size = new_phrase.len_sections();
-            new_phrase = new_phrase.bulk_reduce_halves(&size_checker, &confidence_interpreter);
-            match log::max_level() {
-                log::LevelFilter::Info => {
-                    log::info!(
-                        "Schema: {:?}\n# of permutations: {:e}",
-                        new_phrase.convert_to_string(),
-                        new_phrase.permutations()
-                    );
-                }
-                x if x >= log::LevelFilter::Debug => {
-                    log::debug!(
-                        "Schema: {:?}\n# of sections: {}\n# of refs: {}\n# of permutations: {:e}",
-                        new_phrase.sections,
-                        new_phrase.len_sections(),
-                        new_phrase.num_of_references(),
-                        new_phrase.permutations()
-                    );
-                }
-                _ => (),
-            };
+        if let Some((pair_size, last_size)) = recursive_val {
+            // Currently in recursive loop
+            // Collecting section len to determine if ending or not
+            if pair_size < self.len_sections() {
+                self.clone()
+            } else if last_size <= self.len_sections() {
+                self.bulk_halves_to_end(
+                    Some((pair_size + 1, usize::MAX)),
+                    size_checker,
+                    confidence_interpreter,
+                )
+            } else {
+                self.bulk_halves_to_end(
+                    Some((pair_size, self.len_sections())),
+                    size_checker,
+                    confidence_interpreter,
+                )
+            }
+        } else {
+            // Setting up initial recursion
+            self.bulk_halves_to_end(Some((2, usize::MAX)), size_checker, confidence_interpreter)
         }
-        new_phrase
     }
 }
 
